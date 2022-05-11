7c6389d8388e6d958a0a1a4f287d955f467e2d17
Raj shah <getraj98@yahoo.com>
2022-05-11 00:55:51 -0400

    Added models and repositories for backend

---
+++ src/main/java/com/learn/api/repository/VoteRepository.java
@@ -0,0 +1,9 @@
+package com.learn.api.repository;
+
+import com.learn.api.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}
