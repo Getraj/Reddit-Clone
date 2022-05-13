4ed3cd4e2b4914b6a20f6fbff86ef64536627d4b
Raj shah <getraj98@yahoo.com>
2022-05-13 15:10:37 -0400

    Added user registration and service classes with supporting business logic.

---
+++ src/main/java/com/learn/api/exception/SpringRedditException.java
@@ -0,0 +1,8 @@
+package com.learn.api.exception;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/learn/api/service/MailService.java
@@ -0,0 +1,36 @@
+package com.learn.api.service;
+
+import com.learn.api.exception.SpringRedditException;
+import com.learn.api.model.NotificationEmail;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class MailService {
+
+    private final JavaMailSender mailSender;
+    private final MailContentBuilder mailContentBuilder;
+
+    public void sendEmail(NotificationEmail notificationEmail){
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("springreddit@email.com");
+            messageHelper.setTo(notificationEmail.getRecipient());
+            messageHelper.setSubject(notificationEmail.getSubject());
+            messageHelper.setText(mailContentBuilder.build(notificationEmail.getBody()));
+        };
+        try{
+            mailSender.send(messagePreparator);
+            log.info("Activation email sent!");
+        }catch (MailException e){
+            throw new SpringRedditException("Exception occured when sending email to "+notificationEmail.getRecipient());
+        }
+    }
+}
